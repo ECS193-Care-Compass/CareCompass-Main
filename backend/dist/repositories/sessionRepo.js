e005427fce9e4c735eca613a755301b7da7d265d
chris-nguyen18 <chris.nguyensj04@gmail.com>
2026-02-03 13:55:04 -0800

    Ignore CareCompass-Backend folder used for testing

--- backend/dist/repositories/sessionRepo.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createSessionRepo = createSessionRepo;
-const memorySessionRepo_1 = require("./memory/memorySessionRepo");
-function createSessionRepo() {
-    return new memorySessionRepo_1.MemorySessionRepo();
-}
